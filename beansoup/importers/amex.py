69c9f9bc3369b69406a7550c5a55fe4bc66306ee
Filippo Tampieri <fxt@users.noreply.github.com>
2016-04-03 18:52:36 -0400

    Generalized amex.PdfFiler to filing.Importer to file documents based on matching their filename with a regexp.

--- beansoup/importers/amex.py
+++ beansoup/importers/amex.py
@@ -1,84 +1,11 @@
 """Importers for American Express statements."""
 
 import calendar
-import datetime
-import logging
-from os import path
-import re
 
-from beancount.ingest import importer
+from beansoup.importers import filing
 
 
-class PdfFiler(importer.ImporterProtocol):
-    """A limited importer for PDF-format American Express monthly statements.
-
-    This importer is really only a filer. It identifies American Express
-    monthly statements based exclusively on the file name.
-    It is not capable of extracting any transactions, but it can be used
-    to identify and file these monthly statements.
-
-    The expected file name follows the pattern
-    'Statement_MONTHABBR YEAR.pdf'
-    Where MONTHABBR is a 3-character abbreviation of a month, starting with
-    an uppercase letter (e.g. Jan) and YEAR is a 4-digit year.
-    """
-    months = dict((name, i) for i, name in enumerate(calendar.month_abbr))
-    file_name_re = re.compile(
-        '^Statement_(?P<month>%s) (?P<year>\d{4}).pdf$' % '|'.join(
-            calendar.month_abbr[1:]))
-
-    def __init__(self, account, basename=None, first_day=1, closed=False):
-        """Create a new filer for the given account.
-
-        Args:
-          account: An account string, the account to associate to the statements.
-          basename: An optional string, the name of the new files.
-          first_day: An int in [1,28]; the first day of the billing period.
-          closed: A Boolean; if True, the new files will start with
-            the date of the last day of the billing period; otherwise, they
-            will start with the date of the following day.
-        """
-        self.account = account
-        self.basename = basename
-        self.first_day = first_day
-        self.closed = closed
-
-    def name(self):
-        """Include the filing account in the name."""
-        return '{}: "{}"'.format(super().name(), self.file_account(None))
-
-    def identify(self, file):
-        """Identify whether the file can be processed by this importer."""
-        # Match for a compatible MIME type.
-        if (file.mimetype() != 'application/pdf' and
-            file.mimetype() != 'application/x-pdf'):
-            return False
-
-        # Match the file name.
-        return self.file_name_re.match(path.basename(file.name))
-
-    def file_account(self, _):
-        """Return the account associated with the file"""
-        return self.account
-
-    def file_name(self, file):
-        """Return the optional renamed account file name."""
-        if self.basename:
-            return self.basename + path.splitext(file.name)[1]
-
-    def file_date(self, file):
-        """Return the filing date for the file."""
-        matches = self.file_name_re.match(path.basename(file.name))
-        year = int(matches.group('year'))
-        month = self.months[matches.group('month')]
-        date = datetime.date(year, month, self.first_day)
-        if self.closed:
-            date -= datetime.timedelta(days=1)
-        return date
-
-    def extract(self, file):
-        """Do not attempt to extract any transactions from the file."""
-        logging.warning(
-            "Cannot extract from PDF file '{}'. "
-            "Please use a proper importer and data format".format(file.name))
-        return []
+class PdfFilingImporter(filing.Importer):
+    """A filing importer for American Express PDF monthly statements."""
+    filename_regexp = ('^Statement_(?P<month>%s) (?P<year>\d{4}).pdf$' %
+                       '|'.join(calendar.month_abbr[1:]))
---
+++ beansoup/importers/filing.py
@@ -0,0 +1,96 @@
+"""A file-only importer."""
+
+import calendar
+import datetime
+import logging
+from os import path
+import re
+
+from beancount.ingest import importer
+
+from beansoup.utils import period
+
+
+class Importer(importer.ImporterProtocol):
+    """A document-filing class for monthly files; it does not import anything.
+
+    This importer only supports bean-identify and bean-file. It does not
+    extract any transactions; in fact, it does not even open the file.
+    It uses a regular expression to match a filename to an account and to
+    a date (interpreted as the last day of a billing period).
+    """
+    def __init__(self, account, basename=None,
+                 first_day=1, closed=True, filename_regexp=None):
+        """Create a new filing importer for the given account.
+
+        Args:
+          account: An account string, the account to associate to the files.
+          basename: An optional string, the name of the new files.
+          filename_regexp: A regular expression string used to match the
+            basename (no path) of the target file. This regexp should include
+            capturing groups for `year`, `month`, and (optional) `day` of
+            the end of the period covered by the file; if `day` is
+            missing, the `first_day` argument will be used to compute the
+            end date.
+            Example: '^Statement_(?P<month>\w{3}) (?P<year>\d{4}).pdf$'
+          first_day: An int in [1,28]; the first day of the billing period.
+          closed: A Boolean; if True, the new files will start with
+            the date of the last day of the billing period; otherwise, they
+            will start with the date of the following day.
+        """
+        self.filename_re = re.compile(filename_regexp or self.filename_regexp)
+        self.account = account
+        self.basename = basename
+        self.first_day = first_day
+        self.closed = closed
+
+    def name(self):
+        """Include the filing account in the name."""
+        return '{}: "{}"'.format(super().name(), self.file_account(None))
+
+    def identify(self, file):
+        """Identify whether the file can be processed by this importer."""
+        # Match the file name.
+        return self.filename_re.match(path.basename(file.name))
+
+    def file_account(self, _):
+        """Return the account associated with the file"""
+        return self.account
+
+    def file_name(self, file):
+        """Return the optional renamed account file name."""
+        if self.basename:
+            return self.basename + path.splitext(file.name)[1]
+
+    def file_date(self, file):
+        """Return the filing date for the file."""
+        matches = self.filename_re.match(path.basename(file.name))
+        if matches:
+            groups = matches.groupdict()
+            today = datetime.date.today()
+            year = int(groups['year']) if 'year' in groups else today.year
+            month = period.month_number(groups.get('month')) or today.month
+            if 'day' in groups:
+                # The filename fully specifies the document date
+                day = int(groups['day'])
+                date = datetime.date(year, month, day)
+            else:
+                # Use the first day of the billing cycle to compute the
+                # last day of the period for the given year and month
+                if self.first_day > 1:
+                    date = (datetime.date(year, month, self.first_day) -
+                            datetime.timedelta(days=1))
+                else:
+                    _, month_last_day = calendar.monthrange(year, month)
+                    date = datetime.date(year, month, day)
+
+            if not self.closed:
+                date += datetime.timedelta(days=1)
+            return date
+
+    def extract(self, file):
+        """Do not attempt to extract any transactions from the file."""
+        logging.warning(
+            "Cannot extract entries from file '{}'. "
+            "Please use a proper importer and data format".format(file.name))
+        return []
