7ab0f2d70db18d75dc2adae80c4c2f5736eba57a
Filippo Tampieri <fxt@users.noreply.github.com>
2016-05-05 00:23:17 -0400

    Adjusted docs/conf.py to work with readthedocs.org

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import beansoup.version
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
@@ -82,7 +82,7 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'tests', 'museum']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
